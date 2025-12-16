cd608c7c67c40b340777bde922c278108727443f
Arkhavy <arkhavy@gmail.com>
2025-12-16 15:09:05 +0100

    [08] Add completed exercise, all tests PASS
    Bruteforcing the code and not using JS native functions yet tho..

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...numberToRemove) {
+	for (const arg of numberToRemove) {
+		for (let i = array.length - 1; i >= 0; i--) {
+			if (array[i] === arg) {
+				array.splice(i, 1);
+			}
+		}
+	}
+	return (array);
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+/**
+ * TODO :
+ * find ?
+ * map ?
+ * findIndex ?
+ * How to use multiple arguments in callback functions -> arguments array or for of loop
+ * Syntax of function expression and their use beside the dynamic aspect
+ */
