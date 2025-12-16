46f1f3a1aad152162e41cb715397bde91c72e4d1
Arkhavy <arkhavy@gmail.com>
2025-12-16 16:29:41 +0100

    [11] Add completed exercise, all tests PASS

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemperature) {
+	const celsiusTemperature = (fahrenheitTemperature - 32) * 5/9;
+	return (parseFloat(celsiusTemperature.toFixed(1)));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemperature) {
+	const fahrenheitTemperature = celsiusTemperature * 9/5 + 32;
+	return (parseFloat(fahrenheitTemperature.toFixed(1)));
 };
 
 // Do not edit below this line
