cd8509c04c0656199bbddf9349756e04de4f4887
Arkhavy <arkhavy@gmail.com>
2025-12-16 15:24:01 +0100

    [08] Removed note comments

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -11,12 +11,3 @@ const removeFromArray = function(array, ...numberToRemove) {
 
 // Do not edit below this line
 module.exports = removeFromArray;
-
-/**
- * TODO :
- * find ?
- * map ?
- * findIndex ?
- * How to use multiple arguments in callback functions -> arguments array or for of loop
- * Syntax of function expression and their use beside the dynamic aspect
- */
