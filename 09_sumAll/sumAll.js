c794c39f14f589d89571fb3737d9c836f938b8e7
Arkhavy <arkhavy@gmail.com>
2025-12-16 15:55:30 +0100

    [09] Add completed exercise, all tests PASS

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+function checkArguments(first, second) {
+	// check non-number and non-array
+	if (typeof(first) !== "number" || typeof(second) !== "number") {
+		return (true);
+	}
 
+	// check non-positive
+	if (first < 0 || second < 0) {
+		return (true);
+	}
+
+	// check non-integer
+	if (first !== Math.floor(first) || second !== Math.floor(second)) {
+		return (true);
+	}
+	return (false);
+}
+
+const sumAll = function(first, second) {
+	if (checkArguments(first, second)) {
+		return ("ERROR");
+	}
+	let min = first < second ? first : second;
+	let max = first > second ? first : second;
+	let sum = 0;
+	for (let i = min; i <= max; i++) {
+		sum += i;
+	}
+	return (sum);
 };
 
 // Do not edit below this line
