ff243a50e170182254c54f318e4380b5f264b192
Arkhavy <arkhavy@gmail.com>
2026-02-02 18:11:08 +0100

    [14] Add completed exercise, all tests PASS

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(numberGiven) {
+	const count = parseInt(numberGiven);
+	if (count < 0) {
+		return ("OOPS");
+	}
+	if (count == 0) {
+		return (0);
+	}
+	let previous = 0;
+	let current = 1;
+	let next = 1;
+	for (let i = 1; i < count; i++) {
+		next = previous + current;
+		previous = current;
+		current = next;
+	}
+	return (next);
 };
 
 // Do not edit below this line
