0a74a0c9eda330e8d6efb5b06bb57e6b4e290a92
Arkhavy <arkhavy@gmail.com>
2026-02-02 17:56:55 +0100

    [13] Add completed exercise, all tests PASS

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+	const alphanum = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+	const loweredStr = str.toLowerCase();
+	const arrayStr = loweredStr.split("");
+	const cleanArrayStr = arrayStr.filter((char) => {
+		return (alphanum.includes(char));
+	});
+
+	
+	const newStr = cleanArrayStr.join("");
+	const reverseArrayStr = cleanArrayStr.reverse();
+	const reverseStr = reverseArrayStr.join("");
+	if (newStr === reverseStr) {
+		return (true);
+	}
+	return (false);
 };
 
 // Do not edit below this line
