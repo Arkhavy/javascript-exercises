f9a4a751e815ab797a474a69e61c7a8502efb2c8
Arkhavy <arkhavy@gmail.com>
2026-02-02 16:07:14 +0100

    [12] Add completed exercise, all tests PASS
    Some functions are not really like the solution, but works :D

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return (a + b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return (a - b);
 };
 
-const sum = function() {
-	
+const sum = function(numberArray) {
+	let sum = 0;
+	for (let i = 0; i < numberArray.length; i++) {
+		sum += numberArray[i];
+	}
+	return (sum);
 };
 
-const multiply = function() {
-
+const multiply = function(numberArray) {
+	let product = 1;
+	for (let i = 0; i < numberArray.length; i++) {
+		product *= numberArray[i];
+	}
+	return (product);
 };
 
-const power = function() {
-	
+const power = function(number, exponent) {
+	let result = number;
+	for (let i = 1; i < exponent; i++) {
+		result *= number;
+	}
+	return (result);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0) {
+		return (1);
+	}
+	for (let i = number - 1; i > 0; i--) {
+		number *= i;
+	}
+	return (number);
 };
 
 // Do not edit below this line
