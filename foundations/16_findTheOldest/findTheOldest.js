ad5816a47c52253130302c8f807e1238a1ac6e2c
Arkhavy <arkhavy@gmail.com>
2026-02-04 13:22:46 +0100

    [16] Add completed exercise, all tests PASS
    Got a different solution on this one, but still close to it :D

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+	people = people.map((person) => {
+		return ({
+			name: person.name,
+			age: person.yearOfDeath === undefined
+				? new Date().getFullYear() - person.yearOfBirth
+				: person.yearOfDeath - person.yearOfBirth,
+		})
+	});
+	const oldestPerson = people.reduce((oldest, current) => {
+		return (oldest.age > current.age ? oldest : current);
+	});
+	return (oldestPerson);
 };
 
 // Do not edit below this line
